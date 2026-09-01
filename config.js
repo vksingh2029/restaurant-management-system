0c820c5243955875f69694b5169e3be40089713f
vksingh2029 <vksingh6034@gmail.com>
2026-09-02 01:06:39 +0530

    Renamed login to index - updated all references

--- config.js
+++ config.js
@@ -4,8 +4,4 @@
 
 // 🔥 Production (Render URL – apna Render URL daalein)
 const API_BASE = "https://restaurant-management-system-nz13.onrender.com/api";
-
-// 🧪 Development (Local testing – comment out for production)
-// const API_BASE = "http://localhost:3000/api";
-
 export default API_BASE;
